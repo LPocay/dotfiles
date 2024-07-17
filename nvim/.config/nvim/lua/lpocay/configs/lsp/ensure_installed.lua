0511a596beaa3b263d1a41620d683fdb1241b9d2
Luigi Pocay <luigiapocay92@gmail.com>
2024-07-17 17:37:29 -0300

    added alacritty config, modified others

--- nvim/.config/nvim/lua/lpocay/configs/lsp/ensure_installed.lua
+++ nvim/.config/nvim/lua/lpocay/configs/lsp/ensure_installed.lua
@@ -1,3 +1,12 @@
 return {
-  "tsserver", "clangd", "neocmake", "unocss", "rust_analyzer", "tailwindcss", "svelte", "lua_ls", "gopls"
+  "tsserver",
+  "clangd",
+  "neocmake",
+  "unocss",
+  "rust_analyzer",
+  "tailwindcss",
+  "svelte",
+  "lua_ls",
+  "gopls",
+  "pyright"
 }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
-config.font_size = 18
-config.color_scheme = 'Kanagawa (Gogh)'
+config.font_size = 16
+config.color_scheme = 'kanagawabones'
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
   left = '0cell',
