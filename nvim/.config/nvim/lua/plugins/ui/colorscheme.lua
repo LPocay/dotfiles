d75d29db42fdb9304d1508a2c2a3623fb8fc7fd9
Luigi Pocay <luigiapocay92@gmail.com>
2026-04-21 07:34:40 -0300

    Fix groups in kanagawa theme

--- nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,6 +1,50 @@
 return {
   "rebelot/kanagawa.nvim",
   config = function()
+    require("kanagawa").setup({
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+      overrides = function(colors)
+        local theme = colors.theme
+        local makeDiagnosticColor = function(color)
+          local c = require("kanagawa.lib.color")
+          return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+        end
+        return {
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
+
+          DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
+          DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
+          DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
+          DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
+
+          BlinkCmpMenu = { bg = theme.ui.bg_p1 },
+          BlinkCmpLabelDetail = { bg = colors.palette.dragonBlack3 },
+          BlinkCmpMenuSelection = { bg = colors.palette.waveBlue1 },
+          BlinkCmpMenuBorder = { fg = "", bg = "" },
+
+					-- LineNr = { fg = "#C0A36E", bg = "NONE" },
+					CursorLineNr = { fg = colors.palette.sakuraPink, bg = "NONE" },
+        }
+      end,
+    })
     vim.cmd("colorscheme kanagawa-dragon")
   end,
 }
